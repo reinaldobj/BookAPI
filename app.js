494d49213cf68ab56fb9f3510c927e99620588d3
Reinaldo Bispo Pereira Junior <reinaldob_j@hotmail.com>
2018-01-14 18:10:07 -0200

    GET e POST concluídos

--- app.js
+++ app.js
@@ -1,8 +1,24 @@
-var express = require('express');
+var express = require('express'),
+    mongoose = require('mongoose'),
+    bodyParser = require('body-parser');
+
+var db = mongoose.connect('mongodb://localhost/bookAPI');
+
+var Book = require('./models/bookModel');
 
 var app = express();
 
 var port = process.env.PORT || 3000;
+
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(bodyParser.json());
+
+bookRouter = require('./Routes/bookRoutes')(Book);
+
+app.use('/api/books', bookRouter);
+// app.use('/api/authors', authorRouter);
 
 app.get('/', getCallback);
 
